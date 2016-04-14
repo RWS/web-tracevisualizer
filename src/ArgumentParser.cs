27bb7a199ba5961ff6759031328124282798a454
Likhan Siddiquee <lsiddiquee@sdl.com>
2016-04-14 17:14:46 +0200

    Prepping for isolated build

---
+++ src/ArgumentParser.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Tridion.Logging;
+
+namespace Sdl_Web.TraceVisualizer
+{
+    internal static class ArgumentParser
+    {
+        internal static TraceInstruction Parse(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                ShowHelp();
+                return null;
+            }
+
+            try
+            {
+                var instruction = new TraceInstruction();
+                var argsQueue = new Queue<string>(args);
+
+                while (argsQueue.Count > 0)
+                {
+                    string paramName = GetParameterName(argsQueue);
+
+                    switch (paramName)
+                    {
+                        case "-filepath":
+                            string path = GetParameterValue(argsQueue);
+                            instruction.LogPath = path;
+                            break;
+                        case "-tracelevel":
+                            string traceLevel = GetParameterValue(argsQueue);
+                            instruction.TraceLevel = (TraceLevels)Enum.Parse(typeof(TraceLevels), traceLevel);
+                            break;
+                        case "-tracekeywords":
+                            string keywords = GetParameterValue(argsQueue);
+                            instruction.TraceKeywords = (TraceKeywords)Enum.Parse(typeof(TraceKeywords), keywords);
+                            break;
+                        case "-processnames":
+                            string processNames = GetParameterValue(argsQueue);
+                            instruction.ProcessNames = processNames.Split(',').Select(s => s.Trim());
+                            break;
+                        case "-channelnames":
+                            string channelNames = GetParameterValue(argsQueue);
+                            IEnumerable<TraceChannels> traceChannels = channelNames.Split(',').Select(name => (TraceChannels)Enum.Parse(typeof(TraceChannels), name.Trim()));
+                            instruction.TraceChannels = traceChannels.ToList();
+                            break;
+                        case "-tracefileformat":
+                            string fileFormat = GetParameterValue(argsQueue);
+                            instruction.TraceFileFormat = (FileFormat)Enum.Parse(typeof(FileFormat), fileFormat);
+                            break;
+                        case "-circularsize":
+                            instruction.CircularSize = int.Parse(GetParameterValue(argsQueue));
+                            break;
+                        case "-multifilesize":
+                            instruction.MultiFileSize = int.Parse(GetParameterValue(argsQueue));
+                            break;
+                        case "-buffersizemb":
+                            instruction.BufferSizeMb = int.Parse(GetParameterValue(argsQueue));
+                            break;
+                        case "-bufferquantumkb":
+                            instruction.BufferQuantumKB = int.Parse(GetParameterValue(argsQueue));
+                            break;
+                        default:
+                            throw new Exception(string.Format("Unknown parameter name: '{0}'", paramName));
+                    }
+                }
+
+                return instruction;
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine();
+
+                ShowHelp();
+                return null;
+            }
+        }
+
+        private static string GetParameterValue(Queue<string> argsQueue)
+        {
+            string paramValue = argsQueue.Dequeue();
+            if (paramValue[0] == '-')
+            {
+                throw new Exception(string.Format("Unexpected parameter value: '{0}'", paramValue));
+            }
+
+            return paramValue;
+        }
+
+        private static string GetParameterName(Queue<string> argsQueue)
+        {
+            string paramName = argsQueue.Dequeue().ToLowerInvariant();
+            if (paramName[0] != '-')
+            {
+                throw new Exception(string.Format("Unknown parameter name: '{0}'", paramName));
+            }
+
+            return paramName;
+        }
+
+        private static void ShowHelp()
+        {
+            Console.WriteLine("Usage: ");
+        }
+    }
+}
---
+++ src/TraceInstruction.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Tridion.Logging;
+
+namespace Sdl_Web.TraceVisualizer
+{
+    internal class TraceInstruction
+    {
+        private List<TraceChannels> _traceChannels = null;
+        private TraceKeywords? _traceKeywords;
+        private TraceLevels? _traceLevel;
+        
+        internal string LogPath { get; set; }
+
+        internal IEnumerable<string> ProcessNames { get; set; }
+
+        internal List<TraceChannels> TraceChannels
+        {
+            get
+            {
+                return _traceChannels ?? new List<TraceChannels> { Tridion.Logging.TraceChannels.DefaultChannel, Tridion.Logging.TraceChannels.TcmChannel, Tridion.Logging.TraceChannels.TtmChannel };
+            }
+            set
+            {
+                _traceChannels = value;
+            }
+        }
+
+        internal TraceKeywords TraceKeywords
+        {
+            get
+            {
+                return _traceKeywords ?? TraceKeywords.Public | TraceKeywords.Extension | TraceKeywords.External;
+            }
+            set
+            {
+                _traceKeywords = value;
+            }
+        }
+
+        internal TraceLevels TraceLevel
+        {
+            get
+            {
+                return _traceLevel ?? TraceLevels.Verbose;
+            }
+            set
+            {
+                _traceLevel = value;
+            }
+        }
+
+        internal Action OnComplete { get; set; }
+
+        internal Action<Exception> OnError { get; set; }
+
+        internal FileFormat TraceFileFormat { get; set; }
+
+        internal int? CircularSize { get; set; }
+
+        internal int? MultiFileSize { get; set; }
+
+        internal int? BufferSizeMb { get; set; }
+
+        internal int? BufferQuantumKB { get; set; }
+
+        internal bool IsSingleProcessTrace
+        {
+            get
+            {
+                return ProcessNames != null && ProcessNames.Count() == 1;
+            }
+        }
+    }
+
+    public enum FileFormat
+    {
+        Text,
+        Etl
+    }
+}
